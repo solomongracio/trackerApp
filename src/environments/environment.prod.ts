229d4bd5db94612cba884789aa5edef755c79207
solomongracio <solomonvgracio@gmail.com>
2017-09-02 21:53:00 +0530

    issue in prod build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBJqWQueTUJXji1gQpJ8g4K48uC-PvY9eU',
+    authDomain: 'preethihomeapp.firebaseapp.com',
+    databaseURL: 'https://preethihomeapp.firebaseio.com',
+    projectId: 'preethihomeapp',
+    storageBucket: '',
+    messagingSenderId: '569667562116'
+  }
 };
